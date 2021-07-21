8aa397aadfd24e55e8fe275bfc44340b04534fc3
liwll <liwill7778@gmail.com>
2021-07-20 22:42:51 -0400

    Modify styling

--- src/display/createContent.js
+++
@@ -1,8 +0,0 @@
-const createContent = () => {
-    const body = document.querySelector('body');
-    const content = document.createElement('div');
-    content.setAttribute('id', 'content');
-    body.appendChild(content);
-}
-
-export default createContent;
--- src/pages/renderAbout.js
+++ src/pages/renderAbout.js
@@ -4,17 +4,17 @@ const renderAbout = () => {
     aboutPage.classList.add('about');
     aboutPage.innerHTML = `
         <div class="about">
-        <div class="heading">
-            <h1>Enjoy the <span style="color: green">best food</span> in all of the <span style="color: #3c2fc5">Grand Line</span>...</h1>
-        </div>
+            <div class="heading">
+                <h1>Enjoy the <span style="color: green">best food</span> in all of the <span style="color: #3c2fc5">Grand Line</span>...</h1>
+            </div>
             <img id="img-indoors" src="./imgs/baratie.jpg" alt="">
-        <div class="heading">
-            <h1>Sourcing ingredients from the legendary 
-                <span style="color: #003daf">All Blue Sea</span>, and backed by the 
-                <span style="color: #890304">Straw Hat Pirates</span>, 
-                <span style="color: rgb(129, 209, 233);">Restaraunt Baratie's</span> 
-                food is enjoyed by all!</h1>
-        </div>
+            <div class="heading">
+                <h1>Sourcing ingredients from the legendary 
+                    <span style="color: #003daf">All Blue Sea</span>, and backed by the 
+                    <span style="color: #890304">Straw Hat Pirates</span>, 
+                    <span style="color: rgb(129, 209, 233);">Restaraunt Baratie's</span> 
+                    food is enjoyed by all!</h1>
+            </div>
             <img id="img-crew" src="./imgs/baratie_eating.jpg" alt="">
         </div>
     `
